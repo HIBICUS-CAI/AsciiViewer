bf8bbfa5873439c1e8d48a4ffe8f742f3b73479a
Cai Yuanhan <cai_yuanhan@outlook.com>
2025-04-19 08:51:36 +0900

    初期設定ファイルとプロジェクト構成を追加

---
+++ AsciiViewer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AsciiViewer;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+#if DEBUG
+        args.ToList().ForEach(Console.WriteLine);
+        // リモートデバッグ時アタッチするまで待機用
+        if (args.Length >= 1 && int.TryParse(args[0], out int loopCount))
+        {
+            for (int i = 0; i < loopCount; i++)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+#endif
+
+        Console.WriteLine("Hello, World!");
+    }
+}
